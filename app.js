d834ddaffa79c312182769bf89d539ce909cb5f7
rasbabbo <rasbabbo@yahoo.com>
2014-07-22 18:00:02 -0700

    got next page working

--- app.js
+++ app.js
@@ -21,15 +21,13 @@ app.get('/search', function(req, res) {
   });
 });
 
-app.get('/movie', function(req, res) {
-  //req.query is the EXPRESS term to query the url
-  var query = req.query.searchTerm;
-  // res.send("search page: " + query);
+app.get('/movie/:id', function(req, res) {
+  var query = req.params.id;
   var url = "http://www.omdbapi.com/?i=" + query;
   request(url, function(error, response, body) {
     if (!error) {
       var data = JSON.parse(body);
-      res.render("movie.ejs", {movieList: data.Search || [] })
+      res.render("movie.ejs", {movie: data || [] })
     }
   });
 });
